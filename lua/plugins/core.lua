4b5f235d2ac9c63aacd1e690f750f52d26785d54
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-08-21 09:34:15 +0200

    feat: add nvim-silicon cause its cool

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,4 +1,34 @@
 return {
   'nvim-lua/plenary.nvim',
   'dhruvasagar/vim-table-mode',
+
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("nvim-silicon").setup({
+        font = "JetBrains Mono=16",
+        theme = "OneHalfDark",
+
+        pad_horiz = 20,
+        pad_vert = 20,
+
+        line_offset = function(args)
+          return args.line1
+        end,
+
+        to_clipboard = true,
+      })
+    end,
+    keys = {
+      {
+        "<M-s>",
+        function()
+          require("nvim-silicon").clip()
+        end,
+        mode = { 'n', 'v' },
+      },
+    },
+  }
 }

ab62f620fe85cffb0c2f7fb2c91fd9f12074a5e9
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-09-19 11:31:00 +0200

    feat: map <A-r> to GrugFar

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,7 +38,8 @@ map('n', '<A-w>', '<cmd>DiffviewOpen<cr>',          { silent = true })
 map({ 'n', 'v' }, '<A-d>', '"_d', { silent = true })
 map({ 'n', 'v' }, '<A-c>', '"_c', { silent = true })
 
-map("n", "<A-m>", "<cmd>make<CR>")
+map('n', '<A-m>', '<cmd>make<cr>')
+map('n', '<A-r>', '<cmd>GrugFar<cr>', { silent = true })
 
 map('n', '<leader>l', function()
   local is_loclist_open = false

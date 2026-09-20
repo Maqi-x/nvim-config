4aa4d3db08a3ba056c0d2e36da00cef54ad2fe23
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-09-20 09:53:16 +0200

    feat: support local configuration

--- init.lua
+++ init.lua
@@ -1,31 +1,34 @@
 -- advanced multi-file config
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
+require('config.options')
+require('config.keymaps')
+require('config.autocmds')
+
+-- local configuration
+pcall(require, 'local.init')
 
 -- lazy bootstrap
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
---if vim.v.servername == "" then
---  vim.fn.serverstart("/tmp/nvim-" .. vim.fn.getpid())
---end
---if vim.fn.has("nvim") == 1 then
---    vim.env.NVIM_LISTEN_ADDRESS = "/tmp/nvim-" .. vim.fn.getpid()
---end
+local spec = {
+  { import = "plugins" },
+}
+
+local local_plugins = vim.fn.stdpath("config") .. "/lua/local/plugins"
+if vim.uv.fs_stat(local_plugins) then
+  table.insert(spec, { import = "local.plugins" })
+end
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
+  spec = spec,
 })
